95f274f441d515137a0e18a1b9b633f1e69e6ee7
AI-nsley69 <hello@benji.foo>
2024-10-18 10:05:18 +0200

    Final two sum code

--- TwoSum.hs
+++ TwoSum.hs
@@ -2,19 +2,20 @@
 -- output = [0,1] -> indexes -> 2 + 7 = 9
 -- generate sums of each item with the next
 import Data.List
+import Data.Maybe (isNothing, isJust)
 
-twoSum :: Num a => a -> [a] -> (Maybe Int, Maybe Int)
-twoSum =
-    let a = map (target -) nums
-        b = zip nums a
-        c = filter (\(x,y) -> (x > 0) && (y > 0)) b
-        d = head c
-    in (elemIndex (fst d) nums, elemIndex (snd d) nums)
+isPositive :: (Num a, Ord a) => (a, a) -> Bool
+isPositive (a, b) = (a > 0) && (b > 0)
 
-nums = [2,7,11,15]
-target = 9
+isTupleJust :: Num a => (Maybe a, Maybe a) -> Bool
+isTupleJust (a, b) = isJust a && isJust b
+
+tupleElemIndex :: (Num a, Eq a) => [a] -> (a, a) -> (Maybe Int, Maybe Int)
+tupleElemIndex xs (a, b) = (elemIndex a xs, elemIndex b xs)
+
+twoSum :: (Num a, Ord a) => a -> [a] -> (Maybe Int, Maybe Int)
+twoSum target nums = head $ filter isTupleJust $ map (tupleElemIndex nums) $ filter isPositive $ zip nums $ map (target -) nums
 
 main :: IO()
-main = do
-    print (twoSum 9 nums)
+main = print (twoSum 9 [2,7,11,15])
 
